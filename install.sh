ba394aa7d2b536d6161a383a28f266a7cab77fdb
Looby72 <92597781+Looby72@users.noreply.github.com>
2022-05-05 21:50:38 +0200

    changed python to python3.10

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ cd Bot
 wget https://raw.githubusercontent.com/Looby72/PollBot/new-command-structure/Bot/Bot.py
 wget https://raw.githubusercontent.com/Looby72/PollBot/new-command-structure/Bot/poll.py
 wget https://raw.githubusercontent.com/Looby72/PollBot/new-command-structure/Bot/wiki.py
-python -m pip install pyinstaller
-python -m pip install -U disnake
-python -m pip install wikipedia
+python3.10 -m pip install pyinstaller
+python3.10 -m pip install -U disnake
+python3.10 -m pip install wikipedia
 pyinstaller Bot.py
