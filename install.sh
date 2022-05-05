ca86a1dccdf5710c8869ef55858e7ff3871ee920
Looby72 <92597781+Looby72@users.noreply.github.com>
2022-05-05 17:50:32 +0200

    added the new download url

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 rm -rf Bot
 mkdir Bot
 cd Bot
-wget https://raw.githubusercontent.com/Looby72/DiscordBot/new-command-structure/Bot/Bot.py
-wget https://raw.githubusercontent.com/Looby72/DiscordBot/new-command-structure/Bot/poll.py
-wget https://raw.githubusercontent.com/Looby72/DiscordBot/new-command-structure/Bot/wiki.py
-pip install pyinstaller
-python3 -m pip install -U disnake
-python3 -m pip install wikipedia
+wget https://raw.githubusercontent.com/Looby72/PollBot/new-command-structure/Bot/Bot.py
+wget https://raw.githubusercontent.com/Looby72/PollBot/new-command-structure/Bot/poll.py
+wget https://raw.githubusercontent.com/Looby72/PollBot/new-command-structure/Bot/wiki.py
+python -m pip install pyinstaller
+python -m pip install -U disnake
+python -m pip install wikipedia
 pyinstaller Bot.py
