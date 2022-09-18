4ca4b221e34122c2a8b0ce7467f58aa6333f0bfd
Looby72 <92597781+Looby72@users.noreply.github.com>
2022-09-18 19:18:49 +0200

    added new links because paths of python files have changed

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@ rm -rf Bot
 mkdir Bot
 cd Bot
 wget https://raw.githubusercontent.com/Looby72/PollBot/new-command-structure/Bot/Bot.py
-wget https://raw.githubusercontent.com/Looby72/PollBot/new-command-structure/Bot/poll.py
-wget https://raw.githubusercontent.com/Looby72/PollBot/new-command-structure/Bot/wiki.py
-wget https://raw.githubusercontent.com/Looby72/PollBot/new-command-structure/Bot/operations.py
+wget https://raw.githubusercontent.com/Looby72/PollBot/new-command-structure/Bot/poll/classes.py
+wget https://raw.githubusercontent.com/Looby72/PollBot/new-command-structure/Bot/wiki/wiki.py
+wget https://raw.githubusercontent.com/Looby72/PollBot/new-command-structure/Bot/poll/operations.py
 python3.10 -m pip install -U disnake
 python3.10 -m pip install wikipedia
