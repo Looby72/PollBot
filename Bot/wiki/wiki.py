f94c5de442f7422a2880a0793501755bb2b00288
Looby72 <92597781+Looby72@users.noreply.github.com>
2022-10-20 19:15:45 +0200

    improved wikipedia error messages

--- Bot/wiki/wiki.py
+++ Bot/wiki/wiki.py
@@ -1,20 +1,23 @@
 """Defines all Functions which are needed to realize the !wiki command of the Bot"""
-
-import disnake
+#disnake imports
+from disnake import Embed, Colour
+#datetime imports
+from datetime import datetime
+#wikipedia imports
 import wikipedia
-import datetime
 from wikipedia.wikipedia import WikipediaPage
+from wikipedia import WikipediaException
 
 WIKI_DEFAULT_LANG = "en"
 
-def wiki_main(search_phrase: str, lang: str= WIKI_DEFAULT_LANG) -> disnake.Embed:
+def wiki_main(search_phrase: str, lang: str= WIKI_DEFAULT_LANG) -> Embed:
     """Entry method of the wiki module, takes search-phrase and language and returns the discord Embed"""
     
     try:    
         lang_param(lang)
         embed = get_embed(search_phrase)
-    except Exception as err:
-        embed = disnake.Embed(title = "Error", description = str(err), color = 0xf9f8f6, timestamp=datetime.datetime.now())
+    except (WikipediaException , WikiLanguageError) as err:
+        embed = WikiEmbed(description= str(err), title= type(err).__name__)
     
     return embed
 
@@ -25,24 +28,40 @@ def lang_param(langval: str):
     if langval in wikipedia.languages():
         wikipedia.set_lang(langval)
     else:
-        raise Exception("Language '" + langval + "' not supported")
+        raise WikiLanguageError("Language '" + langval + "' not supported.")
 
 def get_embed(searchval: str):
     """Returns the disnake.embed-object (formatted) to show the wikipedia-summary"""
 
+    embed = None
     page = get_page(searchval)
-    string = get_formatted_summary(page)
-    embed = disnake.Embed(title = page.title, description = string, color = 0xcacfc9, timestamp=datetime.datetime.now())
-    thumbnail = get_picture_url(page)
-    if thumbnail != None:
-        embed.set_thumbnail(url=thumbnail)
+
+    if page == None:
+        description = "No Wikikedia article found, based on your search phrase :("
+        title = "Error"
+        embed = WikiEmbed(description= description, title= title)
+    else:
+        string = get_formatted_summary(page)
+        description = string
+        title = page.title
+        thumbnail = get_picture_url(page)
+
+        embed = WikiEmbed(description= description, title= title)
+
+        if thumbnail != None:
+            embed.set_thumbnail(url=thumbnail)
 
     return embed
 
-def get_page(keyword: str):
-    """Uses wikipedia search to return the best wikipedia page-object"""
+def get_page(keyword: str) -> WikipediaPage | None:
+    """Uses wikipedia search to return the best wikipedia page-object. Returns `None` if nothing was found, based on
+    the keyword."""
 
-    title = wikipedia.search(keyword)[0]
+    try:
+        title = wikipedia.search(keyword)[0]
+    except IndexError:
+        return None
+
     return wikipedia.page(title=title, auto_suggest=False)
 
 def get_formatted_summary(page: WikipediaPage):
@@ -66,3 +85,24 @@ def get_picture_url(page: WikipediaPage):
     if len(pictures) > 0:
         return pictures[0]
     else: return None
+
+class WikiEmbed(Embed):
+    """Defines basic format for all Embeds attached to the Wiki Bot Function."""   
+        
+    def __init__(self, description: str, title: str = "") -> None:
+
+        super().__init__()
+        
+        self.description = description
+        self.colour = Colour(0xcacfc9)
+        self.timestamp = datetime.now()
+        self.title = title
+
+class WikiLanguageError(Exception):
+    """Custom Exception-Class. Thrown, when invalid language prefix is passed."""
+
+    def __init__(self, error: str) -> None:
+        self.error_message = error
+
+    def __str__(self) -> str:
+        return self.error_message
