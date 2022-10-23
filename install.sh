20ef6a860a0db5b18f9f96ff8983272b350533a8
Looby72 <92597781+Looby72@users.noreply.github.com>
2022-10-23 13:25:20 +0200

    description comment on how to use

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 rm -rf Bot
-VERSION="v0.2"
+VERSION="v0.2" #exmaple version, change it to the version you want to download from the github release page
 URL="https://github.com/Looby72/PollBot/releases/download/${VERSION}/PollBot.zip"
 FILENAME="Bot${VERSION}.zip"
 curl -L $URL -o $FILENAME
